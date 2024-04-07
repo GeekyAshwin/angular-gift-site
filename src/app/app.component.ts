33c6d12570bbda714a58cbda5a32620306b0417e
anthwal <anthwalarun003@gmail.com>
2024-04-07 20:01:05 +0530

    add: header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,39 @@
-import { Component, Inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LocalStorageService } from './services/local-storage/local-storage.service';
-import { ApiCallService } from './services/api-call/api-call.service';
-import { HttpClient } from '@angular/common/http';
-
-export interface AppTitle {
-  appTitle: string | null;
-}
+import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-gift-site';
-  constructor(
-    @Inject(LocalStorageService)
-    private localStorageService: LocalStorageService,
-    @Inject(ApiCallService)
-    private apiCallService: ApiCallService,
-    @Inject(HttpClient)
-    private httpClient: HttpClient,
-  ) {
-    this.localStorageService
-      .set<AppTitle>('title', { appTitle: this.title })
-      .subscribe((result) => console.log('successfully stored result', result));
-    this.localStorageService
-      .set<AppTitle>('otherTitle', { appTitle: null })
-      .subscribe((result) => console.log('successfully stored', result));
-    this.localStorageService
-      .get<AppTitle>('title')
-      .subscribe((result) => console.log('successfully got', result?.appTitle));
-    this.localStorageService
-      .get<AppTitle>('otherTitle')
-      .subscribe((result) => console.log('successfully got', result?.appTitle));
-    this.localStorageService
-      .remove('title')
-      .subscribe((result) => console.log('successfully removed', result));
-    this.localStorageService
-      .removeAll()
-      .subscribe((result) =>
-        console.log('cleared local storage with result', result),
-      );
-    // const exampleUrl = this.apiCallService.baseUrl();
-    this.httpClient
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .subscribe(console.log);
+  // title = 'angular-gift-site';
+  constructor() {
+    // private httpClient: HttpClient, // @Inject(HttpClient) // private apiCallService: ApiCallService, // @Inject(ApiCallService) // private localStorageService: LocalStorageService, // @Inject(LocalStorageService)
+    // this.localStorageService
+    //   .set<AppTitle>('title', { appTitle: this.title })
+    //   .subscribe((result) => console.log('successfully stored result', result));
+    // this.localStorageService
+    //   .set<AppTitle>('otherTitle', { appTitle: null })
+    //   .subscribe((result) => console.log('successfully stored', result));
+    // this.localStorageService
+    //   .get<AppTitle>('title')
+    //   .subscribe((result) => console.log('successfully got', result?.appTitle));
+    // this.localStorageService
+    //   .get<AppTitle>('otherTitle')
+    //   .subscribe((result) => console.log('successfully got', result?.appTitle));
+    // this.localStorageService
+    //   .remove('title')
+    //   .subscribe((result) => console.log('successfully removed', result));
+    // this.localStorageService
+    //   .removeAll()
+    //   .subscribe((result) =>
+    //     console.log('cleared local storage with result', result),
+    //   );
+    // this.httpClient
+    //   .get('https://jsonplaceholder.typicode.com/posts')
+    //   .subscribe(console.log);
   }
 }
